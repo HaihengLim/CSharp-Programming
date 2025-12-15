a7d429bda3aae5813638c5e8510f854114837804
HaihengLim <haihenglim738@gmail.com>
2025-12-16 06:15:37 +0700

    Push Project

--- ConsoleApp2/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp2/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d18c40f19be3e2b267613baa1ca99fafb68edb18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ded7b78c665c619faceb5c2f40664f94e9410efc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
